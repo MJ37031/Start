c9489d624712ad6265c6b85e96ab29f294243f94
DESKTOP-IJMJ1NM\USER <rlaalswl0520@naver.com>
2025-04-17 21:09:06 +0900

    25305

---
+++ AlgorismHi.cpp
@@ -0,0 +1,36 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+int main()
+{
+    int N, k, x[10001];
+    int i, j, tmp;
+
+    scanf("%d %d", &N, &k);
+
+    
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", &x[i]);
+    }
+
+    for (i = 0; i < N - 1; i++)
+    {
+        for (j = 1 + i; j < N; j++)
+        {
+            if (x[i] < x[j])
+            {
+                tmp = x[i];
+                x[i] = x[j];
+                x[j] = tmp;
+            }
+        }
+    }
+
+    printf("%d", x[k-1]);
+
+    return 0;
+}
+
